cb563e9f396485bff0c74d2ef46f9b5330fb21ab
arvyy <nma.arvydas.silanskas@gmail.com>
2022-05-28 00:40:39 +0300

    fix broken test

--- src/test/scheme/test-mustache.scm
+++ src/test/scheme/test-mustache.scm
@@ -1,7 +1,7 @@
 (test-group
   "Read, convert to json and render all real types"
   (define specs (read-specs "types/index.scm"))
-  (define json-scm (list->vector (map func->json specs)))
+  (define json-scm (list->vector (map index-entry->json specs)))
   (define json-str
     (let ((port (open-output-string)))
       (json-write json-scm port)
