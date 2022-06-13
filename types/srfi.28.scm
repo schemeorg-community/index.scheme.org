9d65ad54fc0310e235e7e14edc426fa358db2524
arvyy <nma.arvydas.silanskas@gmail.com>
2022-06-14 01:39:43 +0300

    add spec-values to srfi 28

--- types/srfi.28.scm
+++ types/srfi.28.scm
@@ -1,3 +1,9 @@
 (((name . format)
   (signature lambda ((string? format-string) obj ...)  string?)
-  (tags pure)))
+  (tags pure)
+  (spec-values
+    (format-string
+      ("~a" "The corresponding value is inserted into the string as if printed with display.")
+      ("~s" "The corresponding value is inserted into the string as if printed with write.")
+      ("~%A" " A newline is inserted.")
+      ("~~" "A tilde '~' is inserted.")))))
