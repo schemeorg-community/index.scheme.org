3b1ead304b1b5cf1ad91264efdae78495c22ff66
arvyy <nma.arvydas.silanskas@gmail.com>
2022-07-26 02:11:20 +0300

    work

---
+++ types/rnrs.mutable-pairs.6.scm
@@ -0,0 +1,4 @@
+(((name . set-car!)
+  (signature lambda ((pair? pair) boj) undefined))
+ ((name . set-cdr!)
+  (signature lambda ((pair? pair) boj) undefined)))
---
+++ types/rnrs.mutable-strings.6.scm
@@ -0,0 +1,4 @@
+(((name . string-set!)
+  (signature lambda ((string? string) (integer? k) (char? char)) undefined))
+ ((name . string-fill!)
+  (signature lambda ((string? string) (char? fill)) undefined)))
