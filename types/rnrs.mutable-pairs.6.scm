cb5e455bcef0aac038053de12fceccadb1e7d53c
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-01-02 01:54:18 +0200

    add descriptions to rnrs mutable-pairs 6

--- types/rnrs.mutable-pairs.6.scm
+++ types/rnrs.mutable-pairs.6.scm
@@ -1,2 +1,6 @@
-(((name . "set-car!") (signature lambda ((pair? pair) boj) undefined))
- ((name . "set-cdr!") (signature lambda ((pair? pair) boj) undefined)))
+(((name . "set-car!")
+  (signature lambda ((pair? pair) obj) undefined)
+  (desc . "Stores obj in the car field of pair. The set-car! procedure returns unspecified values. If an immutable pair is passed to set-car!, an exception with condition type &assertion should be raised."))
+ ((name . "set-cdr!")
+  (signature lambda ((pair? pair) obj) undefined)
+  (desc . "Stores obj in the cdr field of pair. The set-cdr! procedure returns unspecified values. If an immutable pair is passed to set-car!, an exception with condition type &assertion should be raised.")))
