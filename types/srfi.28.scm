951ba90e14f935f53d49fb68c69ec03964225ba3
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-03-12 14:07:14 +0200

    add description to srfi 28

--- types/srfi.28.scm
+++ types/srfi.28.scm
@@ -1,11 +1,12 @@
 (((name . "format")
   (signature lambda ((string? format-string) obj ...) string?)
   (tags pure)
-  (spec-values
-   (format-string
-    ("~a"
-     "The corresponding value is inserted into the string as if printed with display.")
-    ("~s"
-     "The corresponding value is inserted into the string as if printed with write.")
-    ("~%A" " A newline is inserted.")
-    ("~~" "A tilde '~' is inserted.")))))
+  (desc . "Accepts a message template (a Scheme String), and processes it, replacing any escape sequences in order with one or more characters, the characters themselves dependent on the semantics of the escape sequence encountered.
+An escape sequence is a two character sequence in the string where the first character is a tilde '~'. Each escape code's meaning is as follows:
+    ~a The corresponding value is inserted into the string as if printed with display.
+    ~s The corresponding value is inserted into the string as if printed with write.
+    ~% A newline is inserted.
+    ~~ A tilde '~' is inserted.
+
+~a and ~s, when encountered, require a corresponding Scheme value to be present after the format string. The values provided as operands are used by the escape sequences in order. It is an error if fewer values are provided than escape sequences that require them.
+~% and ~~ require no corresponding value.")))
