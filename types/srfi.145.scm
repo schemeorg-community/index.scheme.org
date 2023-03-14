5ee0d045b668cdf6677a9f513700c7cc67dead59
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-03-14 23:09:01 +0200

    add descriptions to srfi 145

--- types/srfi.145.scm
+++ types/srfi.145.scm
@@ -1,1 +1,3 @@
-(((name . "assume") (signature syntax-rules () ((_ obj message) *))))
+(((name . "assume") 
+  (signature syntax-rules () ((_ obj message) *))
+  (desc . "This special form is an expression that evaluates to the value of obj if obj evaluates to a true value. It is an error if obj evaluates to a false value. In this case, implementations are encouraged to report this error together with the messages to the user, at least when the implementation is in debug or non-optimizing mode. In case of reporting the error, an implementation is also encouraged to report the source location of the source of the error.")))
