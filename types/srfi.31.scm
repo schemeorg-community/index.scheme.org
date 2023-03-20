bf4d9f7fc07fa8b9af08bf83914c5feeb1c3c9ae
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-03-20 20:16:08 +0200

    add description to srfi 31

--- types/srfi.31.scm
+++ types/srfi.31.scm
@@ -3,4 +3,10 @@
    syntax-rules
    ()
    ((_ (name variables ...) body ...) procedure?)
-   ((_ name expression)))))
+   ((_ name expression))))
+ (desc . "(define-syntax rec
+  (syntax-rules ()
+    ((rec (NAME . VARIABLES) . BODY)
+     (letrec ( (NAME (lambda VARIABLES . BODY)) ) NAME))
+    ((rec NAME EXPRESSION)
+     (letrec ( (NAME EXPRESSION) ) NAME))))"))
