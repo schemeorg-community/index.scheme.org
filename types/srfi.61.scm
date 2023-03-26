147d1df5ed8f5ce4309166c7f07108315c8ee6d3
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-03-26 16:52:31 +0300

    add descriptions to srfi 61

--- types/srfi.61.scm
+++ types/srfi.61.scm
@@ -8,4 +8,10 @@
      (test guard => receiver)
      (else expression1 expression2 ...)))
    (guard (value procedure?))
-   (receiver (value procedure?)))))
+   (receiver (value procedure?)))
+  (desc . "The <cond clause> production in the formal syntax of Scheme as written by R5RS in section 7.1.3 is extended with a new option:
+      <cond clause> --->
+          ...
+        | (<generator> <guard> => <receiver>)
+where <generator>, <guard>, & <receiver> are all <expression>s.
+Clauses of this form have the following semantics: <generator> is evaluated. It may return arbitrarily many values. <Guard> is applied to an argument list containing the values in order that <generator> returned. If <guard> returns a true value for that argument list, <receiver> is applied with an equivalent argument list. If <guard> returns a false value, however, the clause is abandoned and the next one is tried.")))
