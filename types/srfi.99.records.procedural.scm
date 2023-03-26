b75b133bdd901fee08ce6d2034392cdc22db826c
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-03-26 16:52:48 +0300

    add descriptions to srfi 99

--- types/srfi.99.records.procedural.scm
+++ types/srfi.99.records.procedural.scm
@@ -3,24 +3,47 @@
    case-lambda
    (((symbol? name) (vector? fieldspecs)) rtd?)
    (((symbol? name) (vector? fieldspecs) ((or rtd? #f) parent)) rtd?))
-  (tags pure))
- ((name . "rtd?") (signature lambda (obj) boolean?) (tags pure predicate))
+  (tags pure)
+  (desc . "name is a symbol, which matters only to the rtd-name procedure of the inspection layer. fieldspecs is a vector of field specifiers, where each field specifier is one of
+* a symbol naming the (mutable) field;
+* a list of the form (mutable name), where name is a symbol naming the mutable field;
+* a list of the form (immutable name), where name is a symbol naming the immutable field.
+
+The optional parent is an rtd or #f. It is an error for any of the symbols in fieldspecs to name more than one of the fields specified by fieldspecs, but the field names in fieldspecs may shadow field names in the parent record-type.
+
+Implementations may wish to extend this procedure to support the non-generative, sealed, and/or opaque features of the R6RS. The recommended way to support those features is to allow any combination of the following arguments to follow the optional parent argument:
+* The symbol sealed means the new rtd cannot be used as the parent of other rtds.
+* The symbol opaque means the record? predicate will not recognize instances of the new rtd.
+* The symbol uid, following by another symbol id, means the new rtd is non-generative with uid id. The semantics of this extension is the same as described by the R6RS.
+
+The recommendation above is not binding on implementations of SRFI 99. There are other ways to realize sealed, opaque, or non-generative rtds.
+Returns an R6RS-compatible record-type descriptor."))
+ ((name . "rtd?") (signature lambda (obj) boolean?) (tags pure predicate) (desc . "Equivalent to the record-type-descriptor? procedure of the R6RS."))
  ((name . "rtd-constructor")
   (signature
    case-lambda
    (((rtd? rtd)) procedure?)
    (((rtd? rtd) (vector? fieldspec)) procedure?))
   (subsigs (return (lambda (fieldvalue ...) record?)))
-  (tags pure))
+  (tags pure)
+  (desc . "rtd is a record-type descriptor, and fieldspecs is an optional vector of symbols.
+If no fieldspecs argument is supplied, then rtd-constructor returns a procedure that expects one argument for each field of the record-type described by rtd and returns an instance of that record-type with its fields initialized to the corresponding arguments. Arguments that correspond to the fields of the record-type's parent (if any) come first.
+If fieldspecs is supplied, then rtd-constructor returns a procedure that expects one argument for each element of fieldspecs and returns an instance of the record-type described by rtd with the named fields initialized to the corresponding arguments.
+It is an error if some symbol occurs more than once in fieldspecs. Fields of a derived record-type shadow fields of the same name in its parent; the fieldspecs argument cannot be used to initialize a shadowed field."))
  ((name . "rtd-predicate")
   (signature lambda ((rtd? rtd)) procedure?)
   (subsigs (return (lambda (obj) boolean?)))
-  (tags pure))
+  (tags pure)
+  (desc . "Equivalent to the record-predicate procedure of the R6RS."))
  ((name . "rtd-accessor")
   (signature lambda ((rtd? rtd) (symbol? field)) procedure?)
   (subsigs (return (lambda ((record? rec)) *)))
-  (tags pure))
+  (tags pure)
+  (desc . "field is a symbol that names a field of the record-type described by the record-type descriptor rtd. Returns a unary procedure that accepts instances of rtd (or any record-type that inherits from rtd) and returns the current value of the named field.
+Fields in derived record-types shadow fields of the same name in a parent record-type."))
  ((name . "rtd-mutator")
   (signature lambda ((rtd? rtd) (symbol? field)) procedure?)
   (subsigs (return (lambda ((record? rec) value) undefined)))
-  (tags pure)))
+  (tags pure)
+  (desc . "field is a symbol that names a field of the record-type described by the record-type descriptor rtd. Returns a binary procedure that accepts instances of rtd (or any record-type that inherits from rtd) and a new value to be stored into the named field, performs that side effect, and returns an unspecified value.
+Fields in derived record-types shadow fields of the same name in a parent record-type.")))
--- types/srfi.99.records.syntactic.scm
+++ types/srfi.99.records.syntactic.scm
@@ -10,4 +10,23 @@
            (field-name)
            (field-name accessor-name)
            (field-name accessor-name modifier-name)))
-   (parent (value rtd?)))))
+   (parent (value rtd?)))
+  (desc . "The semantics of a record type definition is the same as in SRFI 9: the record type definition macro-expands into a cluster of definitions that
+* define the <type name> as the record-type descriptor for the new record-type;
+* defines a constructor for instances of the new record-type (unless the constructor spec is #f);
+* defines a predicate that recognizes instances of the new record-type and its subtypes (unless the predicate spec is #f);
+* defines an accessor for each field name;
+* defines a mutator for each mutable field name.
+
+An ERR5RS record type definition extends SRFI 9 with the following additional options:
+* If a <parent> expression is specified, then it must evaluate to an rtd that serves as the parent record-type for the record-type being defined.
+* If #f is specified for the constructor or predicate, then no constructor or predicate procedure is defined. (This is useful when the record-type being defined will be used as an abstract base class.)
+* If #t is specified for the constructor or predicate, then the name of the constructor is the type name prefixed by make-, and the name of the predicate is the type name followed by a question mark (?).
+* If the constructor name is specified as #t or as an identifier, then the constructor's arguments correspond to the fields of the parent (if any) followed by the new fields added by this record-type definition.
+* If a field spec consists of a single identifier, then
+ - the field is immutable;
+ - the name of its accessor is the type name followed by a hyphen (-) followed by the field name.
+* If a field spec consists of a list of one identifier, then
+ - the field is mutable;
+ - the name of its accessor is the type name followed by a hyphen (-) followed by the field name;
+ - the name of its mutator is the type name followed by a hyphen (-) followed by the field name followed by -set!.")))
