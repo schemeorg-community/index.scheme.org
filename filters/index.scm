80d1b2f15ff6e223b874952523b99201136fbf25
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-04-02 12:54:09 +0300

    switch to only show common filters

--- filters/index.scm
+++ filters/index.scm
@@ -1,4 +1,6 @@
-(((code . "r5rs")
+(
+
+ ((code . "r5rs")
   (name . "R5RS")
   (file . "filters/r5rs.scm"))
 
@@ -22,88 +24,4 @@
   (name . "R7RS large + SRFIs")
   (file . "filters/r7rs-portable.scm"))
 
- ((code . "bigloo")
-  (name . "Bigloo")
-  (file . "filters/bigloo.scm"))
-
- ((code . "chez")
-  (name . "ChezScheme")
-  (file . "filters/chez.scm"))
-
- ((code . "chibi")
-  (name . "Chibi")
-  (file . "filters/chibi.scm"))
-
- ((code . "chicken")
-  (name . "Chicken")
-  (file . "filters/chicken.scm"))
-
- ((code . "cyclone")
-  (name . "Cyclone")
-  (file . "filters/cyclone.scm"))
-
- ((code . "gambit")
-  (name . "Gambit")
-  (file . "filters/gambit.scm"))
-
- ((code . "gauche")
-  (name . "Gauche")
-  (file . "filters/gauche.scm"))
-
- ((code . "gerbil")
-  (name . "Gerbil")
-  (file . "filters/gerbil.scm"))
-
- ((code . "guile")
-  (name . "Guile")
-  (file . "filters/guile.scm"))
-
- ((code . "iron")
-  (name . "IronScheme")
-  (file . "filters/iron.scm"))
-
- ((code . "kawa")
-  (name . "Kawa")
-  (file . "filters/kawa.scm"))
-
- ((code . "larceny")
-  (name . "Larceny")
-  (file . "filters/larceny.scm"))
-
- ((code . "loko")
-  (name . "Loko")
-  (file . "filters/loko.scm"))
-
- ((code . "mit")
-  (name . "MIT")
-  (file . "filters/mit.scm"))
-
- ((code . "racket")
-  (name . "Racket")
-  (file . "filters/racket.scm"))
- 
- ((code . "sagittarius")
-  (name . "Sagittarius")
-  (file . "filters/sagittarius.scm"))
- 
- ((code . "scheme48")
-  (name . "Scheme48")
-  (file . "filters/scheme48.scm"))
- 
- ((code . "stklos")
-  (name . "STKlos")
-  (file . "filters/stklos.scm"))
-
- ((code . "unsyntax")
-  (name . "Unsyntax")
-  (file . "filters/unsyntax.scm"))
- 
- ((code . "vicare")
-  (name . "Vicare")
-  (file . "filters/vicare.scm"))
- 
- ((code . "ypsilon")
-  (name . "Ypsilon")
-  (file . "filters/ypsilon.scm"))
- 
- )
+)
---
+++ filters/index_with_impls.scm
@@ -0,0 +1,109 @@
+(((code . "r5rs")
+  (name . "R5RS")
+  (file . "filters/r5rs.scm"))
+
+ ((code . "r6rs")
+  (name . "R6RS")
+  (file . "filters/r6rs.scm"))
+
+ ((code . "r6rs_all")
+  (name . "R6RS + SRFIs")
+  (file . "filters/r6rs-portable.scm"))
+
+ ((code . "r7rs_small")
+  (name . "R7RS small")
+  (file . "filters/r7rs-small.scm"))
+
+ ((code . "r7rs_large")
+  (name . "R7RS large")
+  (file . "filters/r7rs-large.scm"))
+
+ ((code . "r7rs_all")
+  (name . "R7RS large + SRFIs")
+  (file . "filters/r7rs-portable.scm"))
+
+ ((code . "bigloo")
+  (name . "Bigloo")
+  (file . "filters/bigloo.scm"))
+
+ ((code . "chez")
+  (name . "ChezScheme")
+  (file . "filters/chez.scm"))
+
+ ((code . "chibi")
+  (name . "Chibi")
+  (file . "filters/chibi.scm"))
+
+ ((code . "chicken")
+  (name . "Chicken")
+  (file . "filters/chicken.scm"))
+
+ ((code . "cyclone")
+  (name . "Cyclone")
+  (file . "filters/cyclone.scm"))
+
+ ((code . "gambit")
+  (name . "Gambit")
+  (file . "filters/gambit.scm"))
+
+ ((code . "gauche")
+  (name . "Gauche")
+  (file . "filters/gauche.scm"))
+
+ ((code . "gerbil")
+  (name . "Gerbil")
+  (file . "filters/gerbil.scm"))
+
+ ((code . "guile")
+  (name . "Guile")
+  (file . "filters/guile.scm"))
+
+ ((code . "iron")
+  (name . "IronScheme")
+  (file . "filters/iron.scm"))
+
+ ((code . "kawa")
+  (name . "Kawa")
+  (file . "filters/kawa.scm"))
+
+ ((code . "larceny")
+  (name . "Larceny")
+  (file . "filters/larceny.scm"))
+
+ ((code . "loko")
+  (name . "Loko")
+  (file . "filters/loko.scm"))
+
+ ((code . "mit")
+  (name . "MIT")
+  (file . "filters/mit.scm"))
+
+ ((code . "racket")
+  (name . "Racket")
+  (file . "filters/racket.scm"))
+ 
+ ((code . "sagittarius")
+  (name . "Sagittarius")
+  (file . "filters/sagittarius.scm"))
+ 
+ ((code . "scheme48")
+  (name . "Scheme48")
+  (file . "filters/scheme48.scm"))
+ 
+ ((code . "stklos")
+  (name . "STKlos")
+  (file . "filters/stklos.scm"))
+
+ ((code . "unsyntax")
+  (name . "Unsyntax")
+  (file . "filters/unsyntax.scm"))
+ 
+ ((code . "vicare")
+  (name . "Vicare")
+  (file . "filters/vicare.scm"))
+ 
+ ((code . "ypsilon")
+  (name . "Ypsilon")
+  (file . "filters/ypsilon.scm"))
+ 
+ )
