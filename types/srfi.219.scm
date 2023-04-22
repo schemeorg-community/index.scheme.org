f16a65b0b9bc8404850de478b9642f10ddd3844b
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2023-04-22 10:02:05 +0300

    add descriptions to srfi 219

--- types/srfi.219.scm
+++ types/srfi.219.scm
@@ -10,4 +10,18 @@
    ((_ ((variable inner-param1 ... . inner-rest) outter-param1 ...) body))
    ((_
      ((variable inner-param1 ... . inner-rest) outter-param1 ... . outter-rest)
-     body)))))
+     body)))
+  (desc . "The shorthand version of define behaves as follows:
+(define symbol expr)
+defines symbol with the value expr
+
+(define (symbol . args) expr ...)
+defines symbol with the value (lambda args expr ...)
+
+(define ((symbol . args1) . args) expr ...)
+defines symbol with the value (lambda args1 (lambda args expr ...))
+
+(define (((symbol . args2) . args1) . args) expr ...)
+defines symbol with the value (lambda args2 (lambda args1 (lambda args expr ...)))
+
+and so on.")))
